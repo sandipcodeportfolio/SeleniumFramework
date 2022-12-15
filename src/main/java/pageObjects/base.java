14427e6bb13958236fc160ddac3f1f8b579192fb
sandip <mailstosandip@gmail.com>
2022-12-15 13:59:04 +0530

    1.change geckodriver to cromedriver. 2.POM Selenium Version

--- src/main/java/pageObjects/base.java
+++ src/main/java/pageObjects/base.java
@@ -32,7 +32,7 @@ public class base {
 
 		if (browserName.equals("chrome")) {
 			// setup Chrome WebDriver
-			System.setProperty("chromedriver.chrome.driver",System.getProperty("user.dir")+"/src/main/java/resources/geckodriver.exe");
+			System.setProperty("chromedriver.chrome.driver",System.getProperty("user.dir")+"/src/main/java/resources/chromedriver");
 			ChromeOptions options = new ChromeOptions();
 			options.setPageLoadStrategy(PageLoadStrategy.NONE);
 			driver = new ChromeDriver(options);
